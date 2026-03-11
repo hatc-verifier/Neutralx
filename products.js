e0cb3e3e37bc2d2d9c6ddf86b9b02b810fa74a44
hatc-verifier <sagorhossain35337533@gmail.com>
2026-03-12 00:08:51 +0300

    Update products.js

--- products.js
+++ products.js
@@ -2,7 +2,7 @@ const myProducts = [
     {
         id: 1,
         name: "Premium Look Side Mirror",
-        price: "৳ ১,৮৫০",
+        price: "৳ ১,৮৯৯",
         image: "1001.jpg",
         stock: "In Stock", // এখান থেকে স্টক পরিবর্তন করুন
         details: "মোটরসাইকেলের জন্য স্টাইলিশ এবং হাই-ভিজিবিলিটি সাইড মিরর।"
@@ -10,7 +10,7 @@ const myProducts = [
     {
         id: 2,
         name: "Shadow Comfort Hand Grip",
-        price: "৳ ৯৫০",
+        price: "৳ ৮৯৯",
         image: "1002.jpg",
         stock: "In Stock",
         details: "লং রাইডের জন্য আরামদায়ক হ্যান্ড গ্রিপ। এটি হাতের ঘাম আটকায়।"
@@ -18,7 +18,7 @@ const myProducts = [
     {
         id: 3,
         name: "CNC Adjustable Lever Set",
-        price: "৳ ৩,২০০",
+        price: "৳ ৩,২৯৯",
         image: "1003.jpg",
         stock: "Sold Out", // পরিবর্তন করে দেখালাম
         details: "অ্যাডজাস্টেবল ব্রেক এবং ক্লাচ লিভার সেট।"
@@ -26,7 +26,7 @@ const myProducts = [
     {
         id: 4,
         name: "Full Control Master Kit",
-        price: "৳ ৫,৫০০",
+        price: "৳ ৫,৫৯৯",
         image: "1004.jpg",
         stock: "In Stock",
         details: "হাই-পারফরম্যান্স ব্রেকিং সিস্টেম এবং লিভারের কম্বো সেট।"
